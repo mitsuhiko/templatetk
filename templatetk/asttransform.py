80120f00bd7283a59b8954bba8cb5ec1957db7c8
Armin Ronacher <armin.ronacher@active-4.com>
2011-06-22 15:15:02 +0200

    Started work on identifier tracking

--- templatetk/asttransform.py
+++ templatetk/asttransform.py
@@ -20,6 +20,8 @@
 """
 from __future__ import with_statement
 
+from binascii import hexlify, unhexlify
+
 from .nodeutils import NodeVisitor
 
 
@@ -30,16 +32,79 @@ except ImportError:
     have_ast = False
 
 
+_context_target_map = {
+    'store':        ast.Store,
+    'param':        ast.Param,
+    'load':         ast.Load
+}
+
+
+class IdentManager(object):
+
+    def __init__(self):
+        self.index = 0
+
+    def next_num(self):
+        num = self.index
+        self.index += 1
+        return num
+
+    def override(self, name):
+        return '%s_%s' % (self.encode(name), self.next_num())
+
+    def encode(self, name):
+        return 'l_%s' % hexlify(name)
+
+    def decode(self, name):
+        if name[:2] != 'l_':
+            return False
+        return unhexlify(name.split('_', 1)[0])
+
+    def iter_identifier_maps(self, start):
+        ptr = start
+        while ptr is not None:
+            yield ptr.local_identifiers
+            ptr = ptr.parent
+
+    def temporary(self):
+        return 't_%d' % self.next_num()
+
+
 class FrameState(object):
 
-    def __init__(self, config):
+    def __init__(self, config, parent=None, scope='soft',
+                 ident_manager=None):
+        assert scope in ('soft', 'hard'), 'unknown scope type'
         self.config = config
-        self.parent = None
+        self.parent = parent
+        self.scope = scope
+        self.local_identifiers = {}
+        self.required_aliases = {}
+        self.ident_manager = ident_manager
+
+    def derive(self, scope='soft'):
+        return self.__class__(self.config, self, scope, self.ident_manager)
+
+    def lookup_name(self, name, ctx='load'):
+        assert ctx in _context_target_map, 'unknown context'
+        for idmap in self.ident_manager.iter_identifier_maps(self):
+            if name in idmap:
+                local_identifier = idmap[name]
+                if idmap is not self.local_identifiers:
+                    old = local_identifier
+                    self.local_identifiers[name] = local_identifier = \
+                        self.ident_manager.override(name)
+                    self.required_aliases[local_identifier] = old
+                return local_identifier
+
+        local_identifier = self.ident_manager.encode(name)
+        self.local_identifiers[name] = local_identifier
+        return local_identifier
 
 
 def to_ast(node, config):
     transformer = ASTTransformer(config)
-    return transformer.transform(node, FrameState(config))
+    return transformer.transform(node)
 
 
 class ASTTransformer(NodeVisitor):
@@ -49,9 +114,10 @@ class ASTTransformer(NodeVisitor):
         if not have_ast:
             raise RuntimeError('Python 2.6 or later required for AST')
         self.config = config
+        self.ident_manager = IdentManager()
 
-    def transform(self, node, fstate):
-        assert fstate.config is self.config, 'config mismatch'
+    def transform(self, node):
+        fstate = FrameState(self.config, ident_manager=self.ident_manager)
         return self.visit(node, fstate)
 
     def visit_block(self, nodes, state):
@@ -66,9 +132,10 @@ class ASTTransformer(NodeVisitor):
                     result.extend(rv)
         return result
 
-    def make_config_call(self, _method, args=None):
-        return ast.Call(ast.Attribute(ast.Name('config', ast.Load()), ast.Load()),
-                        [self.visit(x) for x in (args or ())], [], None, None)
+    def make_config_call(self, method, args, fstate):
+        return ast.Call(ast.Attribute(ast.Name('config', ast.Load()),
+                        method, ast.Load()),
+                        [self.visit(x, fstate) for x in (args or ())], [], None, None)
 
     def make_render_func(self, name, lineno=None):
         body = [ast.Assign(ast.Name('config', ast.Store()),
@@ -77,6 +144,16 @@ class ASTTransformer(NodeVisitor):
         funcargs = ast.arguments([ast.Name('rtstate', ast.Param())], None,
                                  None, [])
         return ast.FunctionDef(name, funcargs, body, [], lineno=lineno)
+
+    def make_target_context(self, ctx):
+        return _context_target_map[ctx]()
+
+    def inject_scope_code(self, fstate, body):
+        before = []
+        for alias, old_name in fstate.required_aliases.iteritems():
+            before.append(ast.Assign(ast.Name(alias, 'store'),
+                                     ast.Name(old_name, 'load')))
+        body[:] = before + body
 
     def visit_Template(self, node, fstate):
         rv = ast.Module(lineno=1)
@@ -87,5 +164,38 @@ class ASTTransformer(NodeVisitor):
 
     def visit_Output(self, node, fstate):
         for child in node.nodes:
-            yield ast.Expr(self.make_config_call('to_unicode', [child]),
+            yield ast.Expr(self.make_config_call('to_unicode', [child], fstate),
                            lineno=child.lineno)
+
+    def visit_For(self, node, fstate):
+        loop_fstate = fstate.derive()
+        target = self.visit(node.target, fstate)
+        iter = self.visit(node.iter, fstate)
+        did_iterate = self.ident_manager.temporary()
+        rv = ast.For(target, iter, [ast.Assign(ast.Name(did_iterate, ast.Store()),
+                                               ast.Name(True, ast.Load()))], [],
+                     lineno=node.lineno)
+        rv.body.extend(self.visit_block(node.body, loop_fstate))
+        self.inject_scope_code(loop_fstate, rv.body)
+        # TODO: else_
+        return [ast.Assign(ast.Name(did_iterate, ast.Store()),
+                           ast.Name(False, ast.Name())), rv]
+
+    def visit_Name(self, node, fstate):
+        name = fstate.lookup_name(node.name)
+        ctx = self.make_target_context(node.ctx)
+        return ast.Name(name, ctx)
+
+    def visit_Const(self, node, fstate):
+        val = node.value
+        if isinstance(val, (int, float, long)):
+            return ast.Num(val)
+        elif isinstance(val, basestring):
+            return ast.Str(val)
+        elif isinstance(val, tuple):
+            return ast.Tuple([self.visit(x, fstate) for x in val])
+        elif isinstance(val, list):
+            return ast.List([self.visit(x, fstate) for x in val])
+        elif val in (None, True, False):
+            return ast.Name(str(val), ast.Load())
+        assert 0, 'Unsupported constant value for compiler'
